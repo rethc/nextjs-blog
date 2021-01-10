8184716dcd69e39903c91c88c6f36dc80ac52ceb
rethc <reth@xtra.co.nz>
2021-01-11 08:46:28 +1300

    test commit

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
         <p>I am an aspiring software developer who enjoys creating web applications and solving problems. </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          <a href="https://nextjs.org/learn">our Next.js tutorial.</a>.)
         </p>
       </section>
     </Layout>
